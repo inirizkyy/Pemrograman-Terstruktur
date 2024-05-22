cf396f329aecf3e79e1e5a295ff5eaaa7a80e794
inirizkyy <152785592+inirizkyy@users.noreply.github.com>
2024-05-22 10:29:14 +0700

    Add files via upload

---
+++ binary search.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int binary_search(int arr[], int n, int val) {
+    int left = 0;
+    int right = n-1;
+    int mid;
+
+    while (left <= right) {
+        mid = (left + right) / 2;
+
+        if (arr[mid] == val)
+            return mid;
+
+        if (arr[mid] > val)
+            left = mid - 1;
+
+        else
+            right = mid + 1;
+    }
+    return -1;
+}
+
+int main() {
+    int n;
+    int arr[100];
+    int val;
+
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin>>arr[i];
+    }
+
+    cin >> val;
+    cout << binary_search(arr, n, val) << endl;
+}
